7b9ffd8a8ef46830d29e6fd87e99713b02fdee41
sophiecheav <sophie.cheavseang@gmail.com>
2020-08-21 11:54:49 +0200

    add data.js for test + update Carte component

---
+++ src/router/data.js
@@ -0,0 +1,35 @@
+export const project = [
+  {
+    id: 1,
+    image: 'http://lorempixel.com/400/200/people/1',
+    project_title: 'Banane',
+    firstname: 'Youpi',
+    lastname: 'Youhouhou',
+    category: 'Social',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    financial_needs: '15000',
+    other_needs: 'Stratégie marketing'
+  },
+  {
+    id: 2,
+    image: 'http://lorempixel.com/400/200/people/2',
+    project_title: 'Poney',
+    firstname: 'Sarah',
+    lastname: 'Rubio',
+    category: 'Éducation',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    financial_needs: '35000',
+    other_needs: 'Direction artistique'
+  },
+  {
+    id: 3,
+    image: 'http://lorempixel.com/400/200/people/3',
+    project_title: 'Chamouraï',
+    firstname: 'Sophie',
+    lastname: 'Cheav-Seang',
+    category: 'Mixité',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    financial_needs: '80000',
+    other_needs: 'Relations publiques'
+  },
+]
