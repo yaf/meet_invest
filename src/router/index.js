c666b8a0558558eb65c2594b6ad2f84b8ee09ab0
Sarah Rubio <sarah@air-de-sarah.home>
2020-08-21 11:22:07 +0200

    delete fonctionnement.vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Accueil from '@/views/accueil.vue'
 import APropos from '@/views/aPropos.vue'
 import EntrepreneurDetails from '@/views/entrepreneurDetails.vue'
 import Entrepreneurs from '@/views/entrepreneurs.vue'
-import Fonctionnement from '@/views/fonctionnement.vue'
 import FormContact from '@/views/formContact.vue'
 import FormEntrepreneur from '@/views/formEntrepreneur.vue'
 import FormInvestisseur from '@/views/formInvestisseur.vue'
@@ -33,11 +32,6 @@ Vue.use(VueRouter)
     path: '/entrepreneurs',
     name: 'Entrepreneurs',
     component: Entrepreneurs,
-  },
-  {
-    path: '/',
-    name: 'Fonctionnement',
-    component: Fonctionnement,
   },
   {
     path: '/',
